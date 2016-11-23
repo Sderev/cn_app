21678cf7b6c3c68626e733166acf9c5f3a627741
freddylimpens <freddy.limpens@gmail.com>
2016-11-23 17:30:44 +0100

    drafted the next tests and the way to test HTML generation

--- tests/tests.py
+++ tests/tests.py
@@ -19,22 +19,27 @@ class ModuleParsingTestCase(unittest.TestCase):
 
     def setUp(self):
         """ Build up control_module from tests.config.json file and sample_module from parsing of module_test.md"""
-        self.longMessage=True # for more verbose exception messages
+        #self.longMessage=True # for more verbose exception messages
         with open('tests.config.json', encoding='utf-8') as jsf:
-            self.control_module = json.load(jsf, object_hook=lambda d: namedtuple('X', d.keys())(*d.values())) #we use namedtuple for convenience in coding style
+            self.control_module = json.load(jsf, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
 
         with open("module_test.md", encoding='utf-8') as sample_file:
             self.sample_object = model.Module(sample_file, "tests", "http://culturenumerique.univ-lille3.fr")
             self.sample_object.toHTML(False)
-            outfile = open('sample.config.json', 'wb')
-            outfile.write(self.sample_object.toJson())
+            # outfile = open('sample.config.json', 'wb')
+            # outfile.write(self.sample_object.toJson())
             self.sample_module = json.loads(self.sample_object.toJson(), object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+        del self.sample_object
 
+    def tearDown(self):
+        print("-- tearing down %s --" % self.__class__)
+        self.control_module = None
+        self.sample_module = None
 
     def check_number_of_sections(self):
         """for  sample module 'sample_m', match the number of sections with control module 'control_m'"""
         self.assertEqual(len(self.control_module.sections),len(self.sample_module.sections), "Not the same number of sections")
-        print("-- Number of sections OK --")
+        print("[ModuleParsingTestCase]-- Number of sections OK --")
 
 
     def check_headers_parsing(self):
@@ -42,8 +47,8 @@ class ModuleParsingTestCase(unittest.TestCase):
         pass
 
 
-    def check_subsections_split_and_types(self):
-        """Loop through all subsections and check types and source split
+    def check_subsections(self):
+        """Loop through all subsections and check
             we catch and keep each type of exception to separate checks
         """
         typeMatchingError = None
@@ -69,15 +74,15 @@ class ModuleParsingTestCase(unittest.TestCase):
                 except AssertionError as htmlSrcMatchingError:
                     pass
         if not typeMatchingError:
-            print("-- Subsections types OK --")
+            print("[ModuleParsingTestCase]-- Subsections types OK --")
         else:
             raise typeMatchingError
         if not srcSplitError:
-            print("-- Subsections split OK --")
+            print("[ModuleParsingTestCase]-- Subsections split OK --")
         else:
             raise srcSplitError
         if not htmlSrcMatchingError:
-            print("-- Subsections html_src OK --")
+            print("[ModuleParsingTestCase]-- Subsections html_src OK --")
         else:
             raise htmlSrcMatchingError
 
@@ -85,25 +90,25 @@ class ModuleParsingTestCase(unittest.TestCase):
         #check number of sections
         self.check_number_of_sections()
         #check subsections are splited right and with right types
-        self.check_subsections_split_and_types()
+        self.check_subsections()
         #Module object exact match
-        self.assertEqual(self.control_module, self.sample_module,msg="Module objects do not match exactly")
-        print("-- Exact match OK --")
+        self.assertEqual(self.control_module, self.sample_module, msg="Module objects do not match exactly")
+        print("[ModuleParsingTestCase]-- Exact match OK --")
 
 
-# class HtmlGenerationTestCase(ModuleParsingTestCase):
-#     """ Basic class for testing output generation in HTML """
-#     def setUp(self):
-#         ModuleParsingTestCase.setUp(self)
-#         pass
-#         # then build html output for control and sample
-#         # module_template = jenv.get_template("module.html")
-#         # module_html_content = module_template.render(module=module)
-#         # html = site_template.render(course=course_obj, module_content=module_html_content, body_class="modules", logo=logo)
-#         # utils.write_file(html, os.getcwd(), outDir, module.module+'.html')
-#
-#     def runTest(self):
-#         pass
+class HtmlGenerationTestCase(ModuleParsingTestCase):
+    """ Basic class for testing output generation in HTML """
+    def setUp(self):
+        super(HtmlGenerationTestCase, self).setUp()
+        # then build html output for control and sample
+        # module_template = jenv.get_template("module.html")
+        # module_html_content = module_template.render(module=module)
+        # html = site_template.render(course=course_obj, module_content=module_html_content, body_class="modules", logo=logo)
+        # utils.write_file(html, os.getcwd(), outDir, module.module+'.html')
+
+    def runTest(self):
+        print("[HtmlGenerationTestCase] (nothing) OK")
+
 
 class XmlComparator():
     """ Class to compare two XML snipets """

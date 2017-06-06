fe92ff10468f4cdf7ed7980ea8bf12a54632dbfc
CelestineSauvage <celestine.sauvage@gmail.com>
2017-06-06 10:47:05 +0200

    coveralls

--- tests/all_test.py
+++ tests/all_test.py
@@ -14,32 +14,3 @@ os.system('python parsergift_test.py')
 os.system('python model_test.py')
 os.system('python edx_test.py')
 os.system('python ims_test.py')
-
-# class RunTest(Thread):
-#
-#     def __init__(self, chiffre):
-#         Thread.__init__(self)
-#         self.chiffre = chiffre
-#
-#     def run(self):
-#         if self.chiffre == 0:
-#             execfile('parsergift_test.py')
-#         if self.chiffre == 1:
-#             execfile('model_test.py')
-#         if self.chiffre == 2:
-#             execfile('edx_test.py')
-#         if self.chiffre == 3:
-#             execfile('ims_test.py')
-#         else :
-#             return
-#
-# thread_1 = RunTest(0)
-# thread_2 = RunTest(1)
-#
-#     # Lancement des threads
-# thread_1.start()
-# thread_2.start()
-#
-#     # Attend que les threads se terminent
-# thread_1.join()
-# thread_2.join()

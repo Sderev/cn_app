e47137b124fa931dcc2d1f8c66836a00008553fd
Marc Tommasi <marc.tommasi@inria.fr>
2017-06-11 00:08:14 +0200

    Allows deployment in several distinct urls: remove absolute urls and allow to relocate static files

--- cn_app/site_settings.template.py
+++ cn_app/site_settings.template.py
@@ -1,18 +1,21 @@
 # SECURITY WARNING: don't run with debug turned on in production!
 import os
-import settings
 
+# Turn off in production environment
 DEBUG = True
 
-ALLOWED_HOSTS = ['escapad.univ-lille3.fr', '127.0.0.1']
+# Website
+ALLOWED_HOSTS = ['localhost.localdomain', '127.0.0.1']
 
 # SQLite conf
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3', # for production give absolute path when using sqlite3
+        # for production give absolute path when using sqlite3
+        'NAME': 'db.sqlite'
     }
 }
+
 # Example with PostGreSQL
 # DATABASES = {
 #     'default': {
@@ -25,22 +28,28 @@ DATABASES = {
 #     }
 # }
 
-# ** DATA DIRECTORIES **
 
-# For a dev env, DATA_DIR may be inside src folder
-#DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'repo-data')
-DATA_URL = 'http://localhost/cnapp_data/'
+# URL for static and data resources
+STATIC_URL = "/static/"
+DATA_URL = "http://example.com/cnapp_data/"
+
 # For a prod env, give absolute paths to folders writable by web server user
 DATA_DIR = '/path/to/repo-data'
+# For a dev env, DATA_DIR may be inside src folder
+# DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+#                         'repo-data')
 
-REPOS_DIR = os.path.join(DATA_DIR,'repositories')
+
+REPOS_DIR = os.path.join(DATA_DIR, 'repositories')
 GENERATED_SITES_DIR = os.path.join(DATA_DIR, 'sites')
 GENERATED_SITES_URL = os.path.join(DATA_URL, 'sites')
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'agoodsecretkey'
 
-# In a productino environment, make sure to give a filename writable by web server
+# In a production environment, make sure to give a
+# filename writable by web server
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -51,7 +60,7 @@ LOGGING = {
         'apps_handler': {
             'level': 'INFO',
             'class': 'logging.FileHandler',
-            'filename': 'debug.log',
+            'filename': 'debug.log', 
         },
     },
     'loggers': {
@@ -65,3 +74,4 @@ LOGGING = {
         },
     },
 }
+
